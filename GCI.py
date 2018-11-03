ff979c77b36a337906e7deb8157273531f7faece
Rohan Lodhi <rohanlodhi619@gmail.com>
2018-11-03 19:53:43 +0530

    Add files via upload

---
+++ GCI.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    print("GCI is Great")
+    i=+1
+
+name = input("Please enter your name: ")
+print("Hello " + name + ", Please to meet you!")
+print("Did you know your name in backwords is " + name[::-1] + "?")
